4ba436a3872def87a15848337861cd7cbf62f7c1
bonavi <bonavii@icloud.com>
2025-11-03 20:24:02 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "ProtoDefinitions",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v12)
+    ],
+    products: [
+        // Это то, что будет доступно извне
+        .library(
+            name: "ProtoDefinitions",
+            targets: ["ProtoDefinitions"]
+        ),
+    ],
+    dependencies: [
+        // Зависимость от SwiftProtobuf
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.26.0"),
+    ],
+    targets: [
+        .target(
+            name: "ProtoDefinitions",
+            dependencies: [
+                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+            ],
+            path: "Sources/Protos"
+        ),
+    ]
+)
