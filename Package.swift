2b23e6d50d3a3af2a43c08cf46642673415dc152
bonavi <bonavii@icloud.com>
2025-11-03 21:14:45 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -1,28 +1,30 @@
-// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "ProtoDefinitions",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v12)
+        .iOS(.v18),
+        .macOS(.v15)
     ],
     products: [
-        // Это то, что будет доступно извне
         .library(
             name: "ProtoDefinitions",
             targets: ["ProtoDefinitions"]
         ),
     ],
     dependencies: [
-        // Зависимость от SwiftProtobuf
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.26.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "2.0.0"),
     ],
     targets: [
         .target(
             name: "ProtoDefinitions",
             dependencies: [
-                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "GRPCCore", package: "grpc-swift"),
+                .product(name: "GRPCProtobuf", package: "grpc-swift"),
+                .product(name: "GRPCHTTP2Core", package: "grpc-swift"),
+                .product(name: "GRPCNIOTransportHTTP2", package: "grpc-swift"),
             ],
             path: "proto"
         ),
